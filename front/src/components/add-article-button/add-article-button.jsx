51ca53f7fb5206e9f2059a9c6bd5485fa134c3cd
Andrey Petrovskiy <andrey.petrovskiy.work@gmail.com>
2020-12-10 13:58:06 +0200

    create component structure in React App

---
+++ front/src/components/add-article-button/add-article-button.jsx
@@ -0,0 +1,11 @@
+import './add-article-button.css';
+
+function AddArticle() {
+  return (
+    <div className="add_article">
+      <span className="add_article_text">Add Article</span>
+    </div>
+  );
+}
+
+export default AddArticle;
